9e8392bc5212425e02604e84da4f9765a96d5c63
Maarten van Gompel <proycon@anaproy.nl>
2022-02-17 23:57:00 +0100

    startserver_development: use pip rather than invoke setup.py directly

--- startserver_development.sh
+++ startserver_development.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ ! -z "$VIRTUAL_ENV" ]; then
-    python setup.py develop
+    pip3 install -e .
 else
-    echo "No virtual environment detected, you have to take care of running python setup.py install or setup.py develop yourself!">&2
+    echo "No virtual environment detected, you have to take care of running pip install yourself!">&2
 fi
 CLAM_HOST=localhost CLAM_PORT=8080 clamservice -d frog_webservice.frog_webservice
