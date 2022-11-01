59f7f511c21be16c2967db6a64ccfb8cfa563ead
Maarten van Gompel <proycon@anaproy.nl>
2022-11-01 11:08:31 +0100

    typo

--- runit.d/uwsgi.run.sh
+++ runit.d/uwsgi.run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# This script is meant to be invoked via runit (installed in /etc/service/nginx/run), not directly
+# This script is meant to be invoked via runit (installed in /etc/service/uwsgi/run), not directly
 
 uwsgi --plugin python3 \
       --uid ${UWSGI_UID:100} \
